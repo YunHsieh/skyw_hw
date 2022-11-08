cdc820a59da2546840b5ec76266be75b52fd4fad
yun <kilicapeto@gmail.com>
2022-11-08 19:26:45 +0800

    add: send message by user

--- socket_io/server.py
+++ socket_io/server.py
@@ -1,31 +1,27 @@
 import socket
 from . import contants
-import utils
-import statistics
 
-def start():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((contants.HOST, contants.PORT))
-    s.listen(5)
 
-    print('server start at: %s:%s' % (contants.HOST, contants.PORT))
-    print('wait for connection...')
+class Socket_Server():
 
-    # received the connect
-    conn, addr = s.accept()
-    print('connected by ' + str(addr))
+    def __init__(self):
+        pass
 
-    indata = conn.recv(1024)
-    print(indata.decode())
+    def start(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((contants.HOST, contants.PORT))
+        s.listen(5)
 
-    conn.settimeout(60)
-    data = input('Server is ready. You can type intergers and then click [ENTER].  Clients will show the mean, median, and mode of the input values:')
-    if not data:
-        conn.send('0'.encode())
+        print('server start at: %s:%s' % (contants.HOST, contants.PORT))
+        print('wait for connection...')
 
-    data = utils.to_nums(data)
-    if data == []:
-        print('only number in your input')
-        conn.send('0')
+        # received the connect
+        self.conn, addr = s.accept()
+        print('connected by ' + str(addr))
 
-    conn.send(str(statistics.mean(data)).encode())
+        indata = self.conn.recv(1024)
+        print(indata.decode())
+
+    def send_msg(self, value):
+        self.conn.settimeout(60)
+        self.conn.send(str(value).encode())
